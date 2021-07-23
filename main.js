dba362fcbab69ae6cc3a61e92eac70419fbe71d7
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-07-23 11:05:27 +0900

    정규식 정리

---
+++ main.js
@@ -0,0 +1,152 @@
+import random from './getRandom'
+
+// 산술 연산자 (arithmetic operator)
+// console.log('--------산술-------')
+
+// console.log(1 + 2)
+// console.log(5 - 7)
+// console.log(3 * 4)
+// console.log(10 / 2)
+// console.log(10 % 3)
+
+// 할당 연산자 (assignment operator)
+// console.log('--------할당-------')
+
+// const a = 2;
+// let b = 3;
+// b = b + 1;
+// b += 1;
+// b -= 1;
+// b *= 2;
+// b /= 2;
+// b %= 3;
+
+// console.log(a, b)
+
+// 비교 연산자 (comparison operator)
+// console.log('--------비교-------')
+
+// const c = 1
+// const d = 1
+
+// const e = 13
+// const f = 13
+
+// console.log(c === d)
+// console.log(e === f)
+
+// function isEqual(x, y) {
+//   return x === y
+// }
+
+// console.log(isEqual(1, 1))
+// console.log(isEqual(2, '2'))
+
+
+// console.log(e !== f)
+// console.log(e < f)
+// console.log(e > f)
+// console.log(e >= f)
+// console.log(e <= f)
+
+// 논리 연산자 (logical operator)
+// console.log('--------논리-------')
+// const aa = 1 === 1
+// const bb = 'AB' === 'AB'
+// const cc = false
+
+// console.log(aa)
+// console.log(bb)
+// console.log(cc)
+
+// console.log(aa && bb && cc)
+// console.log(aa || bb)
+
+// 삼항 연잔사 (ternary operator)
+
+// const aaa = 1 < 2
+
+// if (aaa) console.log('참')
+// else console.log('거짓')
+
+// console.log(aaa ? '참' : '거짓')
+
+
+// 조건문 (If statement)
+
+// console.log(random())
+
+// const num = random()
+
+// if (num === 0) console.log('num is 0')
+// else if (num === 1) console.log('num is 1')
+// else console.log('rest....')
+
+// 조건문 (Switch statement)
+
+// const number = random()
+
+// switch (number) {
+//   case 0:
+//     console.log('num is 0')
+//     break;
+//   case 2:
+//     console.log('num is 2')
+//     break;
+//   case 4:
+//     console.log('num is 4')
+//     break;
+//   default:
+//     console.log('rest....')
+//     break;
+// }
+
+// 반복문 (For statement)
+// for (시작조건; 종료조건; 변화조건) {}
+
+// const ulEl = document.querySelector('ul')
+
+// console.log(ulEl)
+
+// for (let i = 0; i < 10; i++) {
+//   const li = document.createElement('li')
+//   li.textContent = `list-${i + 1}`
+//   if ((i + 1) % 2 === 0) {
+//     li.addEventListener('click', () => {
+//       console.log(li.textContent)
+//     })
+//   }
+//   ulEl.appendChild(li)
+// }
+
+// 변수 유효범위 (Variable Scope)
+// var, let, const
+
+// function scope() {
+//   if (true) {
+//     var a = 123
+//   }
+//   console.log(a)
+// }
+// scope()
+
+// var : 함수레벨 유효범위
+// let, const : 블록레벨 유효범위
+
+// 형 변환 (Type conversion)
+
+// const a = 1
+// const b = '1'
+
+// console.log(a === parseInt(b))
+
+// Truthy(참 같은 값)
+// true, {}, [], 1, 2, 'false', -12, '3.14' ...
+
+// Fulsy(거짓 같은 값)
+// false, '', null, undefined, 0, -0, NaN
+
+
+
+// ---------------7.23-----------------
+
